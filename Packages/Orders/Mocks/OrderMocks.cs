a4aac9b30205648e4c15437ba2e6e9e1c5d0ba84
Lopatovas <lopatovas@gmail.com>
2020-04-25 19:53:48 +0300

    Randomize some status for orders

--- Packages/Orders/Mocks/OrderMocks.cs
+++ Packages/Orders/Mocks/OrderMocks.cs
@@ -8,6 +8,28 @@ namespace Dronai.Packages.Orders.Mocks
 {
     public class OrderMocks
     {
+        private static string RandomizeStatus()
+        {
+            Random rand = new Random();
+            int randomValue = rand.Next(5);
+            switch (randomValue)
+            {
+                case 0:
+                    return OrderStatus.Paid.Value;
+                case 1:
+                    return OrderStatus.Confirmed.Value;
+                case 2:
+                    return OrderStatus.Denied.Value;
+                case 3:
+                    return OrderStatus.PendingPayment.Value;
+                case 4:
+                    return OrderStatus.Delivered.Value;
+                default:
+                    return OrderStatus.Ordered.Value;
+            }
+                
+        }
+
         public static Order GetOrder()
         {
             Random rand = new Random();
@@ -16,7 +38,7 @@ namespace Dronai.Packages.Orders.Mocks
                 Id = rand.Next(1000),
                 AddressFrom = "Laisves Pr. 469, Kedainiai",
                 AddressTo = "Agurku Pl. 15, Kedainiai",
-                Status = OrderStatus.PendingPayment.Value,
+                Status = RandomizeStatus(),
                 DateOfDelivery = new DateTime()
             };
         }
